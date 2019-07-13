c76d802425f921e4af8760e0a7f31b257b93d42a
rohinthmurugan <46491731+rohinthmurugan@users.noreply.github.com>
2019-07-13 14:40:11 +0530

    Add files via upload

---
+++ vowels.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+   
+char ch;
+scanf("%c",&ch);
+if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+{
+    printf("vowel");
+}
+else
+{
+    printf("consonant");
+}
+
+}
