c7aeb289444974f8da4ca8167800aa6f5b2a4563
rohinthmurugan <46491731+rohinthmurugan@users.noreply.github.com>
2019-07-13 15:04:51 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -5,7 +5,7 @@ void main()
    
 char ch;
 scanf("%c",&ch);
-if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u') || (ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
 {
     printf("vowel");
 }
