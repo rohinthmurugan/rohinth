31b346a75e34c0300fd77fc6ee4624745edd9984
rohinthmurugan <46491731+rohinthmurugan@users.noreply.github.com>
2019-07-13 14:04:51 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#<include<conio.h>
+void main()
+{
+int num;
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("Even");
+}
+else if(num%2!=0)
+{
+printf("Odd");
+}
+else
+{
+printf("Invalid");
+}
+}
