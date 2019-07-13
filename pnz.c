4fb66accfe660ec993ead595d14b1bf71df26a7e
rohinthmurugan <46491731+rohinthmurugan@users.noreply.github.com>
2019-07-13 13:22:29 +0530

    Add files via upload

---
+++ pnz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int num;
+scanf("%d",&num);
+if(num>0)
+{
+printf("Positive");
+}
+else if(num<0)
+{
+printf("Negative");
+}
+else
+{
+printf("Zero");
+}
+}
